30986926c99ab5e3156e39fd96ff8a24ac84fb01
girume1 <70520544+girume1@users.noreply.github.com>
2020-09-14 23:17:58 -0700

    update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,7 +6,6 @@ from uniborg.util import admin_cmd
 from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_GIF = "https://t.me/EthioUserBot/14"
 pm_caption = "`Ethio <Userbot> 🇪🇹 IS:` **ONLINE**\n\n"
 pm_caption += "**SYSTEM STATUS**\n"
 pm_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
@@ -21,14 +20,38 @@ pm_caption += "Deploy Your Own : [Repo](https://github.com/girume1/CBA-Userbot)\
 async def friday(alive):
     chat = await alive.get_chat()
     """ For .alive command, check if the bot is running.  """
-    await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
-    await alive.delete()
+try:
+        await _send_alive(message, output)
+    except (FileIdInvalid, FileReferenceEmpty, BadRequest):
+        await _refresh_id()
+        await _send_alive(message, output)
 
-    
-@borg.on(admin_cmd(pattern=r"Alive", allow_sudo=True))
-async def friday(alive):
-    chat = await alive.get_chat()
-    """ For .alive command, check if the bot is running.  """
-    await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
 
-    
+def _parse_arg(arg: bool) -> str:
+    return "enabled" if arg else "disabled"
+
+
+async def _send_alive(message: Message, text: str) -> None:
+    if not (LOGO_ID and LOGO_REF):
+        await _refresh_id()
+    try:
+        await message.client.send_animation(chat_id=message.chat.id,
+                                            animation=LOGO_ID,
+                                            file_ref=LOGO_REF,
+                                            caption=text)
+    except (MediaEmpty, ChatSendMediaForbidden):
+        await message.client.send_message(chat_id=message.chat.id,
+                                          text=text,
+                                          disable_web_page_preview=True)
+
+
+async def _refresh_id():
+    global LOGO_ID, LOGO_REF  # pylint: disable=global-statement
+    try:
+        gif = (await userge.get_messages('EthioUserBot', 14)).animation
+    except ChannelInvalid:
+        LOGO_ID = None
+        LOGO_REF = None
+    else:
+        LOGO_ID = gif.file_id
+        LOGO_REF = gif.file_ref
